cd82be3e7ad5cb0bd157a7d2a8540c920d567ab9
addurisimhadri <adduriappanna@gmail.com>
2020-06-24 17:59:12 +0530

    hi

--- src/main/java/com/sim/main/Main.java
+++ src/main/java/com/sim/main/Main.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-
 	public static void main(String[] args) {		
 		String str="Vodafone-IN";
 		if(str.contains(" ")) str=str.substring(0, str.indexOf(" "));
@@ -16,24 +15,23 @@ public class Main {
 	      list.add(new StringBuffer("alpha"));
 	      list.add(new StringBuffer("beta"));
 	      list.add(new StringBuffer("charlie"));
-	      System.out.println(list);   //[alpha, beta, charlie]
-	      
-	      
+	      System.out.println(list);   //[alpha, beta, charlie]	         
 	 
 	      for (StringBuffer str1 : list) {
 	         str1.append("change!");   // cannot modify "immutable" objects
 	      }
 	      System.out.println(list);   //[alpha, beta, charlie]
-		
-		
+		 
+		 
 		List<String> lst = new ArrayList<>();
 	      lst.add("alpha");
 	      lst.add("beta");
 	      lst.add("charlie");
 	      System.out.println(lst);   //[alpha, beta, charlie]
 	      Collections.sort(lst);
+	      // cannot modify "immutable" objects
 	      for (String str1 : lst) {
-	         str1 += "change!";   // cannot modify "immutable" objects
+	         str1 += "change!";   
 	      }
 	      System.out.println(lst);   //[alpha, beta, charlie]
 	}
